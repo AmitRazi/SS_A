37b97398ee1cda7ef7ed93a0c45a76428421e17a
AmitRazi <115696533+AmitRazi@users.noreply.github.com>
2022-11-13 21:55:45 +0200

    Update NumClass.h

--- NumClass.h
+++ NumClass.h
@@ -1,9 +1,9 @@
 
 #ifndef UNTITLED_NUMCLASS_H
 #define UNTITLED_NUMCLASS_H
-int isArmstrong(int x);
-int isPalindrome(int x);
-int isStrong(int n);
-int isPrime(int n);
+int isArmstrong(int);
+int isPalindrome(int);
+int isStrong(int);
+int isPrime(int);
 #endif //UNTITLED_NUMCLASS_H
 
