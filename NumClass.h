1dde7e72ea8cceb911b070359155583a3a6693bf
AmitRazi <115696533+AmitRazi@users.noreply.github.com>
2022-11-13 21:50:45 +0200

    Update NumClass.h

--- NumClass.h
+++ NumClass.h
@@ -1,15 +1,9 @@
 
 #ifndef UNTITLED_NUMCLASS_H
 #define UNTITLED_NUMCLASS_H
-int count(int);
-int isArmstrong_helper(int x,int y);
 int isArmstrong(int x);
-int *recursive_reverse(int x, int* y);
-int is_palindrome_rec_helper(int x,int y);
 int isPalindrome(int x);
-int *factorial(int n);
 int isStrong(int n);
 int isPrime(int n);
-int *reverse_num(int);
 #endif //UNTITLED_NUMCLASS_H
 
